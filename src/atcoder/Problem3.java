5b84f0c186413d55e494a3adbea9425e5bd3badd
suguru-3u <suguru.13579515@gmail.com>
2025-08-25 08:52:37 +0900

    atcoder-問題3回答

---
+++ src/atcoder/Problem3.java
@@ -0,0 +1,26 @@
+package atcoder;
+
+/*
+ * URL:https://atcoder.jp/contests/abc420/tasks/abc420_a
+ * 問題名：A - What month is it?
+ */
+
+import java.util.Scanner;
+
+public class Problem3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int month = sc.nextInt();
+        int plusMonth = sc.nextInt();
+
+        int result = (month + plusMonth) % 12;
+        if (result == 0) {
+            result = 12;
+        }
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}

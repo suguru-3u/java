a2579331cfca30fe62ce6db32affd65fd0221f13
suguru-3u <suguru.13579515@gmail.com>
2025-09-03 21:48:36 +0900

    atcoder-問題7回答

---
+++ src/atcoder/Problem7.java
@@ -0,0 +1,39 @@
+package atcoder;
+
+/*
+ * https://atcoder.jp/contests/abc416/tasks/abc416_a
+ *
+ * 問題名：A - Vacation Validation
+ * containsを使用する方法もある
+ *
+ */
+
+
+import java.util.Scanner;
+
+public class Problem7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int strCount = sc.nextInt();
+        int begin = sc.nextInt();
+        int end = sc.nextInt();
+        String str = sc.next();
+
+        boolean result = true;
+        for (int i = begin - 1; i < end; i++) {
+            char ch = str.charAt(i);
+            if (ch == 'x') {
+                result = false;
+                break;
+            }
+        }
+
+        if (result) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+
+        sc.close();
+    }
+}

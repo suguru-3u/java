342e473fa47ae249dd3b482643c15aebf08401f9
suguru-3u <suguru.13579515@gmail.com>
2025-09-12 16:17:28 +0900

    atcoder-問題14回答

---
+++ src/atcoder/Problem14.java
@@ -0,0 +1,34 @@
+package atcoder;
+
+/*
+ * https://atcoder.jp/contests/abc410/tasks/abc410_a
+ *
+ * 問題名：A - G1
+ *
+ */
+
+
+import java.util.Scanner;
+
+public class Problem14 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int raceCount = scanner.nextInt();
+        int[] horse = new int[raceCount];
+        for (int i = 0; i < raceCount; i++) {
+            horse[i] = scanner.nextInt();
+        }
+        int targetAge = scanner.nextInt();
+        int participationCount = 0;
+
+        for (int i = 0; i < raceCount; i++) {
+            if (horse[i] >= targetAge) {
+                participationCount++;
+            }
+        }
+
+        System.out.println(participationCount);
+
+        scanner.close();
+    }
+}

1dddb2fccf67537da6977cca1ae805fa17c4d429
suguru-3u <suguru.13579515@gmail.com>
2025-09-16 22:28:45 +0900

    atcoder-問題16回答

---
+++ src/atcoder/Problem16.java
@@ -0,0 +1,34 @@
+package atcoder;
+
+/*
+ * https://atcoder.jp/contests/abc408/tasks/abc408_a
+ *
+ * 問題名：A - Timeout
+ *
+ */
+
+
+import java.util.Scanner;
+
+public class Problem16 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = scanner.nextInt();
+        double limit = scanner.nextInt();
+        int past = 0;
+
+        for (int i = 0; i < count; i++) {
+            int current = scanner.nextInt();
+            if (past != 0) {
+                if(current - past > limit){
+                    System.out.println("No");
+                    return;
+                }
+            }
+            past = current;
+        }
+
+        System.out.println("Yes");
+        scanner.close();
+    }
+}
